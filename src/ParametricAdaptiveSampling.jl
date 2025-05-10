5ad7aca54505f5004299a37252fbfe1d963803f8
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2025-05-10 11:51:57 +0200

    improvements

--- src/ParametricAdaptiveSampling.jl
+++ src/ParametricAdaptiveSampling.jl
@@ -18,7 +18,6 @@ struct Segment{V}
 end
 
 function Segment(f::Function, t1, v1, t2, v2)
-    factor = 0.5
     t_mid = 0.5 * (t1 + t2)
     v_mid = f(t_mid)
     # abs_error = norm(@. v_mid - 0.5 * (v1 + v2))
@@ -61,9 +60,12 @@ function push_range!(range::Range, v)
     end
 end
 
-"Push to a queue and cache the error value. `errfun: x |-> Number`"
-function push_errfun!(queue, errfun, x, args...)
-    push!(queue, (-errfun(x, args...), x))
+"Check error value against tolerance and push if above."
+function maybe_push_errfun!(queue, errfun, tol, x, args...)
+    err = errfun(x, args...)
+    if err > tol
+        push!(queue, (-err, x))
+    end
 end
 
 "Error relative to the range. Component-wise."
@@ -85,83 +87,80 @@ end
 """
 ### Arguments
 - `f::Function`: The parameteric function. Given a Float64 in `[tmin, tmax]` must return an n-element tuple, where n must be constant across t values.
-- `tmin::Float64`: The lower bound of the parameter domain.
-- `tmax::Float64`: The upper bound of the parameter domain.
+- `tmin::Real`: The lower bound of the parameter domain.
+- `tmax::Real`: The upper bound of the parameter domain.
+
+`f` must have well-defined values within `[tmin, tmax]`, including the endpoints.
 
 ### Keyword Arguments
-- `errfun`: Error function `(Segment, range::Tuple{Float64,...}) -> Float64` where `range` is (an estimate of) the value range. Default = relative to range; this is a good default for plotting.
-- `tolerance`: Tolerance for refinement.
-- `min_points::Int`: Minimum number of points to sample initially.
-- `max_points::Int` or `Nothing`: Maximum number of points to sample. Should be `> min_points`. `nothing` means unlimited.
+- `errfun`: Error function `(Segment, ra::Range) -> Float64` where `ra` is (an estimate of) the value range. Default = l-infinity distance component-wise relative to range; this is a good default for plotting.
+- `tol`: Tolerance to `errfun` for refinement. For plotting, this could be 1 / (max vertical/horizontal resolution). Default = 1e-3.
+- `min_points::Integer`: Minimum number of points to sample initially. Pass 2 for no presampling beyond tmin and tmax.
+- `max_points::Real`: Maximum number of points to sample. Should be `>= min_points`. Pass `Inf` for no limit.
 
 ### Notes
-- If `min_points` is low, `range` in `errfun` may be catastrophically imprecise, at least for initial refinement. You probably don't want that.
+- If `min_points` is low, `range` in `errfun` may be catastrophically small, at least for initial refinement. You probably don't want that.
 
 ### Features not yet implemented (SOMEDAY)
-- Allow to give the initial t values directly (to vary the presampling strategy)
-- Allow to configure the refinement strategy in a more detailed way (see Notes).
-- Infinite or open t ranges where we would adaptively sample larger/smaller (towards 0) t values.
-- Drop points that are ultimately not needed if the range expands during refinement.
+- Allow to give the initial t values directly, rather than an implicit linear range (to vary the presampling strategy)
+- Infinite or open t ranges where we would adaptively sample larger/smaller (towards 0) t values. This may be an instance of a more general hook into refinement.
+- Optional penalty for recursion depth (or something like this) to avoid excessive concentration at polar points if `max_points` gets exhausted.
+- Option to drop points that are ultimately not needed if the range expands during refinement.
 """
 function adaptive_sample_parametric(
     f::Function,
-    tmin::Float64,
-    tmax::Float64;
-    tolerance = 1e-3,
+    tmin::Real,
+    tmax::Real;
+    tol = 1e-4,
     errfun = err_relative_range,
-    min_points = 10,
+    min_points::Integer = 10,
     max_points = 4_000,
 )
-    # todo min_points, max_points option for not given. Can we use Inf for max and 2 for min?
-
-    # Queue in error order, so we can stick to `max_points`. We sort again later.
-    # SOMEDAY can we eliminate that by smarter accounting?
-    queue = BinaryHeap{Tuple{Float64,Segment}}(Base.By(first), [])
-
     # Initial uniform sampling
     @assert min_points >= 2
     # SOMEDAY may want to sample a single value first to determine the value type (specifically, )
     ts_init = range(tmin, tmax, length = min_points)
     vs_init = f.(ts_init)
-    # successive pairs of initial points
-    pairs_init = partition(zip(ts_init, vs_init), 2, 1)
-    segments_init = [Segment(f, t1, v1, t2, v2) for ((t1, v1), (t2, v2)) in pairs_init]
+
+    # We store initial points here and append to it as we go (out of order, sorted later)
+    # SOMEDAY smarter accounting to avoid the final sort?
+    ps = zip(ts_init, vs_init) |> collect
+
+    # Initialize range. We'll update as we go.
+    ra = Range(vs_init)
 
     # We use the value at tmax to presample and to understand the dimension.
     # (TODO should we?)
 
-    # Fill this from initial segments. We'll update as we go.
-    ra = Range(vs_init)
-    for s in segments_init
-        push_errfun!(queue, errfun, s, ra)
+    # Queue of pending splits, in error order, so we can stick to `max_points`. We sort again later.
+    # Initialize with pairs of successive points.
+    queue = BinaryHeap{Tuple{Float64,Segment}}(Base.By(first), [])
+    for ((t1, v1), (t2, v2)) in partition(ps, 2, 1)
+        s = Segment(f, t1, v1, t2, v2)
+        maybe_push_errfun!(queue, errfun, tol, s, ra)
     end
-    @debug queue
 
-    # Refinement and commit.
-    committed = []
-    # TODO max_points not working well, getting stuck in infinite loop. Should consider length(queue) as well.
-    # But if we get too many points *this* way, it's kinda sad: alg is clearly stuck.
-    # This could be b/c of (1) polar points and (2) values close to 0.
-    # Maybe we should commit earlier?
-    while !isempty(queue) && length(committed) < max_points
-        (err, s) = pop!(queue)
-        if err <= tolerance
-            push!(committed, (s.t1, s.v1))
-        else
-            s1 = Segment(f, s.t1, s.v1, s.t_mid, s.v_mid)
-            s2 = Segment(f, s.t_mid, s.v_mid, s.t2, s.v2)
-            push_range!(ra, s1.v_mid)
-            push_range!(ra, s2.v_mid)
-            push_errfun!(queue, errfun, s1, ra)
-            push_errfun!(queue, errfun, s2, ra)
-        end
+    # Refinement.
+    while !isempty(queue) && length(ps) < max_points
+        (_, s) = pop!(queue)
+        push!(ps, (s.t_mid, s.v_mid))
+        s1 = Segment(f, s.t1, s.v1, s.t_mid, s.v_mid)
+        s2 = Segment(f, s.t_mid, s.v_mid, s.t2, s.v2)
+        push_range!(ra, s1.v_mid)
+        push_range!(ra, s2.v_mid)
+
+        maybe_push_errfun!(queue, errfun, tol, s1, ra)
+        maybe_push_errfun!(queue, errfun, tol, s2, ra)
     end
-    # Accounting not quite perfect here.
-    push!(committed, (tmax, f(tmax)))
 
-    sort!(committed; by = first)
+    if !isempty(queue)
+        max_err = first(queue)[1]
+        @warn "max_points=$max_points reached with errors above tolerarnce. Max error = $max_err > $tol = tolerance"
+    end
 
-    return map(first, committed), [p[2] for p in committed]
+    # SOMEDAY can we eliminate sorting by smarter accounting?
+    sort!(ps; by = first)
+    return map(first, ps), [p[2] for p in ps]
 end
 
 end # module ParametricAdaptiveSampling
