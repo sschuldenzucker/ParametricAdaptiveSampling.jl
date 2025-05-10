65783885ff63a435e6069eaa3f5483496fff3b70
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2025-05-10 12:25:44 +0200

    various docs and variants

--- src/ParametricAdaptiveSampling.jl
+++ src/ParametricAdaptiveSampling.jl
@@ -1,6 +1,6 @@
 module ParametricAdaptiveSampling
 
-export adaptive_sample_parametric
+export adaptive_sample_parametric, adaptive_sample
 
 using LinearAlgebra: norm
 using DataStructures
@@ -17,16 +17,13 @@ struct Segment{V}
     v2::V
 end
 
-function Segment(f::Function, t1, v1, t2, v2)
+function Segment(f, t1, v1, t2, v2)
     t_mid = 0.5 * (t1 + t2)
     v_mid = f(t_mid)
-    # abs_error = norm(@. v_mid - 0.5 * (v1 + v2))
-    # # SOMEDAY we should use some sane value instead of eps I think. Allow v_mid to be 0.
-    # rel_error = abs_error / max(norm(v_mid), eps())
     Segment(t1, v1, t_mid, v_mid, t2, v2)
 end
 
-function Segment(f::Function, t1, t2)
+function Segment(f, t1, t2)
     Segment(f, t1, f(t1), t2, f(t2))
 end
 
@@ -82,33 +79,36 @@ end
     Range([0.0, 0.0], [1000.0, 1000.0]),
 ) == 0.025
 
-# TODO review type stability, maybe make a function accepting v_tmax as a sample, and maybe use static arrays to capture the length.
-
 """
+    adaptive_sample_parametric(f, tmin, tmax)
+    adaptive_sample_parametric(f, ts_init)
+
 ### Arguments
-- `f::Function`: The parameteric function. Given a Float64 in `[tmin, tmax]` must return an n-element tuple, where n must be constant across t values.
+- `f`: The parameteric function. Given a Float64 in `[tmin, tmax]` must return an n-element tuple, where n must be constant across t values.
 - `tmin::Real`: The lower bound of the parameter domain.
 - `tmax::Real`: The upper bound of the parameter domain.
+- `ts_init::Collection{Real}`: If this form is used, the initial t values are given explicitly. Otherwise (with `tmin` and `tmax`) we do initial sampling according to `min_points`. Must have size at least 2.
 
 `f` must have well-defined values within `[tmin, tmax]`, including the endpoints.
 
 ### Keyword Arguments
 - `errfun`: Error function `(Segment, ra::Range) -> Float64` where `ra` is (an estimate of) the value range. Default = l-infinity distance component-wise relative to range; this is a good default for plotting.
 - `tol`: Tolerance to `errfun` for refinement. For plotting, this could be 1 / (max vertical/horizontal resolution). Default = 1e-3.
-- `min_points::Integer`: Minimum number of points to sample initially. Pass 2 for no presampling beyond tmin and tmax.
-- `max_points::Real`: Maximum number of points to sample. Should be `>= min_points`. Pass `Inf` for no limit.
+- `min_points::Integer`: Minimum number of t values to sample linearly initially. Pass 2 for no presampling beyond tmin and tmax.
+- `max_points::Real`: Maximum number of points to sample. Pass `Inf` for no limit.
 
 ### Notes
 - If `min_points` is low, `range` in `errfun` may be catastrophically small, at least for initial refinement. You probably don't want that.
 
 ### Features not yet implemented (SOMEDAY)
-- Allow to give the initial t values directly, rather than an implicit linear range (to vary the presampling strategy)
 - Infinite or open t ranges where we would adaptively sample larger/smaller (towards 0) t values. This may be an instance of a more general hook into refinement.
 - Optional penalty for recursion depth (or something like this) to avoid excessive concentration at polar points if `max_points` gets exhausted.
+- Some way of detecting and avoiding polar points. Basically a way to say "it's not useful to plot this, better to cut it out".
 - Option to drop points that are ultimately not needed if the range expands during refinement.
+- Optionally, is there some smartness we can do with the derivative of f (calculated using ForwardDiff)?
 """
 function adaptive_sample_parametric(
-    f::Function,
+    f,
     tmin::Real,
     tmax::Real;
     tol = 1e-4,
@@ -118,8 +118,24 @@ function adaptive_sample_parametric(
 )
     # Initial uniform sampling
     @assert min_points >= 2
-    # SOMEDAY may want to sample a single value first to determine the value type (specifically, )
     ts_init = range(tmin, tmax, length = min_points)
+    adaptive_sample_parametric(
+        f,
+        ts_init;
+        tol = tol,
+        errfun = errfun,
+        max_points = max_points,
+    )
+end
+
+function adaptive_sample_parametric(
+    f,
+    ts_init;
+    tol = 1e-4,
+    errfun = err_relative_range,
+    max_points = 4_000,
+)
+    @assert length(ts_init) >= 2
     vs_init = f.(ts_init)
 
     # We store initial points here and append to it as we go (out of order, sorted later)
@@ -128,9 +144,6 @@ function adaptive_sample_parametric(
 
     # Initialize range. We'll update as we go.
     ra = Range(vs_init)
-
-    # We use the value at tmax to presample and to understand the dimension.
-    # (TODO should we?)
 
     # Queue of pending splits, in error order, so we can stick to `max_points`. We sort again later.
     # Initialize with pairs of successive points.
@@ -155,12 +168,52 @@ function adaptive_sample_parametric(
 
     if !isempty(queue)
         max_err = first(queue)[1]
-        @warn "max_points=$max_points reached with errors above tolerarnce. Max error = $max_err > $tol = tolerance"
+        @warn "max_points=$max_points reached with errors above tolerarnce. Max error = $(-max_err) > $tol = tolerance"
     end
 
     # SOMEDAY can we eliminate sorting by smarter accounting?
     sort!(ps; by = first)
     return map(first, ps), [p[2] for p in ps]
+end
+
+"""
+    adaptive_sample(f, xmin, xmax)
+    adaptive_sample(f, xs)
+
+Non-parametric variant of `adaptive_sample_parametric()` for a function f: Number -> Number. See the documentation of that function.
+
+Note: This function is often not needed. In many cases, for plotting, it will be faster to just spam a lot of x values instead. Cases where you _may_ want this function could be:
+
+1. f is slow to evaluate.
+2. You want to limit the number of data points for some later computation step that is slow.
+3. You need very high precision in your output, maybe for a non-plotting use cases.
+
+In cases 2. and 3., you may also want to use custom values and/or a custom error function in the keyword arguments.
+"""
+function adaptive_sample(
+    f,
+    xmin::Real,
+    xmax::Real;
+    tol = 1e-4,
+    errfun = err_relative_range,
+    min_points::Integer = 10,
+    max_points = 4_000,
+)
+    # Initial uniform sampling
+    @assert min_points >= 2
+    xs_init = range(xmin, xmax, length = min_points)
+    adaptive_sample(f, xs_init; tol = tol, errfun = errfun, max_points = max_points)
+end
+
+function adaptive_sample(f, xs; tol = 1e-4, errfun = err_relative_range, max_points = 4_000)
+    f_parametric(x) = (x, f(x))
+    adaptive_sample_parametric(
+        f_parametric,
+        xs,
+        tol = tol,
+        errfun = errfun,
+        max_points = max_points,
+    )
 end
 
 end # module ParametricAdaptiveSampling
